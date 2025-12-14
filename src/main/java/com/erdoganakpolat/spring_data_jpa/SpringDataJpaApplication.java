5e9f967b2c6ad5a5d50e9c85b71b5250ca3e29c2
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-12-14 23:37:23 +0300

    new update

--- src/main/java/com/erdoganakpolat/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/erdoganakpolat/spring_data_jpa/SpringDataJpaApplication.java
@@ -3,9 +3,14 @@ package com.erdoganakpolat.spring_data_jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages= {"com.erdoganakpolat"})
+
 @SpringBootApplication
+@EntityScan(basePackages= {"com.erdoganakpolat"})
+@ComponentScan(basePackages = {"com.erdoganakpolat"})
+@EnableJpaRepositories(basePackages = {"com.erdoganakpolat"})
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
