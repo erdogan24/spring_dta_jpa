f396514c4ad9843736d196129b67b26122f7f791
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-12-16 22:05:47 +0300

    new update

--- src/main/java/com/erdoganakpolat/services/IStudentService.java
+++ src/main/java/com/erdoganakpolat/services/IStudentService.java
@@ -1,7 +1,11 @@
 package com.erdoganakpolat.services;
 
+import java.util.List;
+
 import com.erdoganakpolat.entities.Student;
 
 public interface IStudentService {
 	public Student saveStudent(Student student);
+	
+	public List<Student> getAllStudents();
 }
--- src/main/java/com/erdoganakpolat/services/impl/StudentServiceImpl.java
+++ src/main/java/com/erdoganakpolat/services/impl/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.erdoganakpolat.services.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +14,16 @@ public class StudentServiceImpl implements IStudentService{
 	
 	@Autowired
 	private StudentRepository studentRepository;
+	
 	@Override
 	public Student saveStudent(Student student) {
 	    return studentRepository.save(student);
 		
+	}
+	@Override
+	public List<Student> getAllStudents() {
+		List<Student> studentList = studentRepository.findAll();
+		return studentList;
 	}
 
 }
