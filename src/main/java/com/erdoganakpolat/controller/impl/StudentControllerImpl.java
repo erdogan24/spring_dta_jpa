f73fb9cbfc50ba3f63d4ca3c26d4bbd5dfa77616
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-12-17 23:50:54 +0300

    new update 3

--- src/main/java/com/erdoganakpolat/controller/impl/StudentControllerImpl.java
+++ src/main/java/com/erdoganakpolat/controller/impl/StudentControllerImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,9 +35,9 @@ public class StudentControllerImpl implements IStudentController{
 	public List<Student> getAllStudents() {
 		return studentService.getAllStudents();
 	}
-
+	@GetMapping(path = "/list/{id}")
 	@Override
-	public Student getStudentById(Integer id) {
+	public Student getStudentById(@PathVariable(name = "id") Integer id) {
 		
 		return  studentService.getStudentById(id);
 	}
